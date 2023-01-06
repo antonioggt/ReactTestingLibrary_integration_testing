707fa8aa301f3595168d939752dbc011e918e01a
antonioggt <antonioggt85@gmail.com>
2023-01-06 09:52:23 -0300

    Testa o componente <FavoritePokemon />

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemon } from '../pages';
+
+describe('Testa o componente <FavoritePokemon />', () => {
+  it('', () => {
+    render(FavoritePokemon);
+    const notFoundTxt = screen.getByText('No favorite Pokémon found');
+    const heading = screen.getByRole('heading', {
+      name: /favorite pokémon/i,
+    });
+    expect(notFoundTxt).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
+  });
+});
