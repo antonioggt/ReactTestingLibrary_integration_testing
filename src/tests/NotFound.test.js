c9f65beea2506a005cf2b80577ebb911cdd41fda
antonioggt <antonioggt85@gmail.com>
2023-01-06 13:15:04 -0300

    Testa o componente <NotFound />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente <NotFound />', () => {
+  it('', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(heading).toBeInTheDocument();
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', imgSrc);
+  });
+});
